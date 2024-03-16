0f23a4138a23c79a1273e9a6eef96dfa9952c462
hasker2 <105449424+hasker2@users.noreply.github.com>
2024-03-16 22:17:10 +0200

    Add files via upload

---
+++ cloudmining/bot/mainbot.py
@@ -0,0 +1,166 @@
+import sqlite3
+
+from aiogram.types import *
+from aiogram.utils.keyboard import *
+from aiogram import Router
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.filters import Command, Text, CommandObject
+from aiogram import Bot
+from filters import AdminFilter, SubFilter
+import asyncio
+import aiosqlite
+import sys
+import datetime
+sys.path.append('..')
+from databaseclass import UserDb, RefDb, token
+from aiogram.utils.deep_linking import create_start_link
+import aiohttp
+from datetime import timezone
+
+bot = Bot(token=token, parse_mode="HTML")
+router = Router()
+
+class Addr_set(StatesGroup):
+    addr = State()
+
+address = "assd;dldl"
+
+@router.message(Command(commands=['start']))
+async def greets(message: Message, command: CommandObject):
+    user = UserDb(message)
+    kb = [
+        [
+            KeyboardButton(text="Account"),
+        ],
+        [
+            KeyboardButton(text="Balance")
+        ],
+        [
+            KeyboardButton(text="Withdraw"),
+            KeyboardButton(text="Deposit")
+        ],
+        [
+            KeyboardButton(text="Referals")
+        ]
+    ]
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+    )
+    await bot.send_message(message.chat.id, f'Hello', reply_markup=keyboard)
+    if_new = await user.add_user()
+    try:
+        if command.args and if_new:
+            if (command.args.isdigit()):
+                await UserDb.increase(int(command.args))
+                await bot.send_message(command.args, "You have got a referee.\nYour terashash limit was increased by 5Th!")
+            else:
+                await RefDb.increase(command.args)
+    except sqlite3.IntegrityError as e:
+        pass
+    except Exception as e:
+        print(e)
+
+@router.message(Text(text="Balance"))
+async def refs(message: Message):
+    kurs = []
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json') as response:
+            kurs = await response.json()
+    th = 0.00000006
+    refs = await UserDb.get_refs(message.chat.id)
+    hashrate = 50+(refs*5)
+    date_string = await UserDb.get_creation_time(message.chat.id)
+    datetime_object = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+    time_passed = datetime.datetime.utcnow() - datetime_object
+    balance = format((th*hashrate)*(time_passed.total_seconds()/3600), ".8f")
+    await message.answer(f"🆔 Your balance is:\n{balance} BTC\n{round(float(balance)*float(kurs['bpi']['USD']['rate_float']), 2)}$")
+
+@router.message(Text(text="Deposit"))
+async def refs(message: Message):
+    await message.answer(address)
+
+@router.callback_query(Text(text='generate_wallet'))
+async def generate_wallet(call: CallbackQuery, state: FSMContext):
+    await UserDb.add_wallet(call.message.chat.id)
+    await bot.send_message(call.message.chat.id, "Your wallet has been created")
+
+@router.message(Text(text="Account"))
+async def refs(message: Message):
+    public_key = await UserDb.get_public_key(message.chat.id)
+    buttons = [
+        [
+            InlineKeyboardButton(text="Set Wallet", callback_data="set_wallet"),
+        ],
+    ]
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    refs = await UserDb.get_refs(message.chat.id)
+    addr = ""
+    if public_key == None:
+        addr = "You have not connected your BTC address yet"
+    else:
+        addr = f"Connected Address:\n<code>{public_key}</code>"
+    await message.answer(f"🆔 <code>{message.chat.id}</code>\nTeraHash limit: {50+(refs*5)}\n{addr}", parse_mode="HTML", reply_markup=keyboard)
+
+@router.message(Text(text="Withdraw"))
+async def refs(message: Message):
+    kurs = []
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json') as response:
+            kurs = await response.json()
+    th = 0.00000006
+    refs = await UserDb.get_refs(message.chat.id)
+    hashrate = 50+(refs*5)
+    date_string = await UserDb.get_creation_time(message.chat.id)
+    datetime_object = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+    time_passed = datetime.datetime.utcnow() - datetime_object
+    balance = format((th*hashrate)*(time_passed.total_seconds()/3600), ".8f")
+    public_key = await UserDb.get_public_key(message.chat.id)
+    buttons = [
+        [
+            InlineKeyboardButton(text="Withdraw", callback_data="set_wallet"),
+        ],
+    ]
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    refs = await UserDb.get_refs(message.chat.id)
+    addr = ""
+    if public_key == None:
+        addr = "You have not connected your BTC address yet"
+    else:
+        addr = f"<code>{public_key}</code>"
+    await message.answer(f"You are going to withdraw your money to your connected address:\n{addr}\nYour balance:\n{balance} ({round(float(balance)*float(kurs['bpi']['USD']['rate_float']), 2)}$)", parse_mode="HTML", reply_markup=keyboard)
+
+@router.callback_query(Text(text='cancel'))
+async def refs(call: CallbackQuery, state: FSMContext):
+    await bot.send_message(call.message.chat.id, "Canceled")
+    await state.clear()
+
+@router.callback_query(Text(text='set_wallet'))
+async def refs(call: CallbackQuery, state: FSMContext):
+    buttons = [
+        [
+            InlineKeyboardButton(text="❌Cancel", callback_data="cancel"),
+        ],
+    ]
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    await bot.send_message(call.message.chat.id, "Enter your Bitcoin address", reply_markup=keyboard)
+    await state.set_state(Addr_set.addr)
+
+@router.message(Addr_set.addr)
+async def getid(message: Message, state: FSMContext):
+    await UserDb.add_wallet(message.chat.id, message.text)
+    await message.answer("Your wallet has been successfully added")
+    await state.clear()
+
+@router.message(Text(text="Referals"))
+async def refs(message: Message):
+    refs = await UserDb.get_refs(message.chat.id)
+    await message.answer(f"You can get 5 TeraHash for every referee you invite\nYour referal link: {await create_start_link(bot, str(message.chat.id))}")
+
+
+
+
+
+
+
---
+++ cloudmining/databaseclass.py
@@ -0,0 +1,244 @@
+import asyncpg
+from aiogram.types import Message
+from mnemonic import Mnemonic
+
+from bitcoinlib.wallets import Wallet, wallet_delete
+from bitcoinlib.mnemonic import Mnemonic
+from bitcoinlib.keys import HDKey
+import mnemonic
+import random, string
+
+user = "bohdandemyanchuk"
+password = "AeT8hs9lOLSq"
+database = "neondb"
+host = "ep-weathered-resonance-391969.eu-central-1.aws.neon.tech"
+
+token = '7012652197:AAFIws6SFmEVGnn-KKlP_Gg0R_mZ13vCRkA'
+
+class UserDb:
+    def __init__(self, message: Message):
+        self.message = message
+
+    async def add_user(self):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute(
+                'INSERT INTO users ("id", "name", "surname", "lang", "date") VALUES ($1, $2, $3, $4, $5)',
+                self.message.chat.id, self.message.from_user.first_name, self.message.from_user.last_name, self.message.from_user.language_code, self.message.date.strftime('%Y-%m-%d %H:%M:%S')
+            )
+            return True
+        except asyncpg.exceptions.UniqueViolationError:
+            await con.execute(
+                'UPDATE users SET "lang" = $1, "name" = $2, "surname" = $3 WHERE "id" = $4',
+                self.message.from_user.language_code, self.message.from_user.first_name,
+                self.message.from_user.last_name, self.message.chat.id
+            )
+            return False
+        except Exception as e:
+            pass
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def increase(_id: int):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute(
+                'UPDATE users SET refs = refs + 1 WHERE id = $1',
+                _id
+            )
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def add_wallet(_id: int, addr: str):
+        # passphrase = mnemonic.Mnemonic("english").generate(128)
+        # print(passphrase)
+        # con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        # hdkey = HDKey().from_passphrase(passphrase, encoding="bech32")
+        # addr = hdkey.subkey_for_path("m/84'/0'/0'/0/0").address()
+        # try:
+        #     await con.execute('update users set private_key = $1, public_key = $2  where id = $3', passphrase,
+        #                       addr, _id)
+        # finally:
+        #     await con.close()
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('update users set public_key = $1  where id = $2', addr, _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_refs(_id: int):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            return await con.fetchval('select refs from users where id = $1', _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_creation_time(_id: int):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            return await con.fetchval('select date from users where id = $1', _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_public_key(_id: int):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            return await con.fetchval('select public_key from users where id = $1', _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def statistic():
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            count_result = await con.fetchval('SELECT count(id) FROM users')
+
+            per_result = await con.fetch(
+                'SELECT lang, COUNT(*) as count FROM users GROUP BY lang'
+            )
+            total_count = sum(row['count'] for row in per_result)
+
+            percentages = []
+            for row in per_result:
+                language_code = row['lang']
+                count = row['count']
+                percentage = (count / total_count) * 100
+                percentage_formatted = f"{percentage:.2f}% ({count})"
+                percentages.append(f"{language_code}:{percentage_formatted}")
+
+            result_str = '\n'.join(percentages)
+            return count_result, result_str
+
+        finally:
+            await con.close()
+
+
+    @staticmethod
+    async def get_users():
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            result = await con.fetch("SELECT id FROM users")
+            user_ids = [row["id"] for row in result]
+            return user_ids
+        finally:
+            await con.close()
+
+class RefDb:
+    @staticmethod
+    async def increase(name: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute(
+                'UPDATE ref SET amount = amount + 1 WHERE name = $1',
+                name
+            )
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def add_ref(refname: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('INSERT INTO ref (name) VALUES ($1)', refname)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def reset_ref(refname: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute(
+                'UPDATE ref SET amount = 0 WHERE name = $1', refname
+            )
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_refs():
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            names = await con.fetch("SELECT name FROM ref")
+            result = [name["name"] for name in names]
+            return result
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_ref(name: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            result = await con.fetch('SELECT name, amount FROM ref WHERE name = $1', name)
+            return result
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def delete_ref(name: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('DELETE FROM ref WHERE name = $1', name)
+        finally:
+            await con.close()
+
+
+
+
+
+
+class ChannelDb:
+    cached_data = None
+    @staticmethod
+    async def cash_link_id():
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            result = await con.fetch('SELECT link, id FROM op')
+            ret = []
+            for i in result:
+                ret.append([i['link'], i['id']])
+            ChannelDb.cached_data = ret
+        finally:
+            await con.close()
+    @staticmethod
+    async def get_link_id():
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            result = await con.fetch('SELECT link, id FROM op')
+            return result
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def delete_channel(_id):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('DELETE FROM op WHERE id = $1', _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def add_channel(_id: int, link: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('INSERT INTO op (link, id) VALUES ($1, $2)', link, _id)
+        finally:
+            await con.close()
+
+class BotDb:
+    @staticmethod
+    async def sql_execute(query):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+
+        try:
+            result = await con.fetch(query)
+            return result
+        finally:
+            await con.close()
+
+
+
+
