3694c9746b99fa0ddc63d5e64cd3ddca5f3f4a92
hasker2 <105449424+hasker2@users.noreply.github.com>
2024-03-16 22:58:35 +0200

    Update main.py

--- cloudmining/main.py
+++ cloudmining/main.py
@@ -5,8 +5,8 @@ from admin import mainadm, mailing, count, sql, senddb, op, ref
 from bot import mainbot
 from aiogram.fsm.storage.memory import MemoryStorage
 from databaseclass import ChannelDb, token
-
-
+from flask import Flask
+import threading
 
 async def main():
     bot = Bot(token=token)
@@ -21,12 +21,22 @@ async def main():
         op.router,
         ref.router,
         mainbot.router
-               ]
+    ]
     for i in routers:
         dp.include_router(i)
     #await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
 
 if __name__ == "__main__":
+    # Start asyncio event loop in the main thread
     asyncio.run(main())
+
+    # Start Flask app in a separate thread
+    thread = threading.Thread(target=lambda: app.run(host='0.0.0.0', port=5000))
+    thread.start()
