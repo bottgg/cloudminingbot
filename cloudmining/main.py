1842be2a4e2d6a7ac8bf9bda74c5949bf9863cb0
hasker2 <105449424+hasker2@users.noreply.github.com>
2024-03-17 15:21:49 +0200

    Update main.py

--- cloudmining/main.py
+++ cloudmining/main.py
@@ -5,13 +5,13 @@ from admin import mainadm, mailing, count, sql, senddb, op, ref
 from bot import mainbot
 from aiogram.fsm.storage.memory import MemoryStorage
 from databaseclass import ChannelDb, token
-from flask import Flask
-import threading
+from background import keep_alive
 
 async def main():
     bot = Bot(token=token)
     dp = Dispatcher(storage=MemoryStorage())
     await ChannelDb.cash_link_id()
+    keep_alive()
     routers = [
         mainadm.router,
         mailing.router,
@@ -36,7 +36,3 @@ def index():
 if __name__ == "__main__":
     # Start asyncio event loop in the main thread
     asyncio.run(main())
-
-    # Start Flask app in a separate thread
-    thread = threading.Thread(target=lambda: app.run(host='0.0.0.0', port=5000))
-    thread.start()
