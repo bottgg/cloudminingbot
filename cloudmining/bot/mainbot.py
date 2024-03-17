cefa687bd10635235345d1395ca34e9573cf448d
hasker2 <105449424+hasker2@users.noreply.github.com>
2024-03-17 21:08:46 +0200

    Update mainbot.py

--- cloudmining/bot/mainbot.py
+++ cloudmining/bot/mainbot.py
@@ -1,5 +1,4 @@
 import sqlite3
-
 from aiogram.types import *
 from aiogram.utils.keyboard import *
 from aiogram import Router
@@ -7,24 +6,18 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.filters import Command, Text, CommandObject
 from aiogram import Bot
-from filters import AdminFilter, SubFilter
-import asyncio
-import aiosqlite
 import sys
 import datetime
 sys.path.append('..')
 from databaseclass import UserDb, RefDb, token
 from aiogram.utils.deep_linking import create_start_link
 import aiohttp
-from datetime import timezone
 
 bot = Bot(token=token, parse_mode="HTML")
 router = Router()
 
 class Addr_set(StatesGroup):
     addr = State()
-
-address = "assd;dldl"
 
 @router.message(Command(commands=['start']))
 async def greets(message: Message, command: CommandObject):
@@ -35,10 +28,6 @@ async def greets(message: Message, command: CommandObject):
         ],
         [
             KeyboardButton(text="Balance")
-        ],
-        [
-            KeyboardButton(text="Withdraw"),
-            KeyboardButton(text="Deposit")
         ],
         [
             KeyboardButton(text="Referals")
@@ -77,75 +66,30 @@ async def refs(message: Message):
     balance = format((th*hashrate)*(time_passed.total_seconds()/3600), ".8f")
     await message.answer(f"🆔 Your balance is:\n{balance} BTC\n{round(float(balance)*float(kurs['bpi']['USD']['rate_float']), 2)}$")
 
-@router.message(Text(text="Deposit"))
-async def refs(message: Message):
-    await message.answer(address)
-
-@router.callback_query(Text(text='generate_wallet'))
-async def generate_wallet(call: CallbackQuery, state: FSMContext):
-    await UserDb.add_wallet(call.message.chat.id)
-    await bot.send_message(call.message.chat.id, "Your wallet has been created")
-
 @router.message(Text(text="Account"))
 async def refs(message: Message):
     public_key = await UserDb.get_public_key(message.chat.id)
     buttons = [
         [
-            InlineKeyboardButton(text="Set Wallet", callback_data="set_wallet"),
+            InlineKeyboardButton(text="🔗Set Wallet", callback_data="set_wallet"),
         ],
+        [
+            InlineKeyboardButton(text="📥Deposit", callback_data="deposit"),
+            InlineKeyboardButton(text="📤Withdraw", callback_data="withdraw"),
+        ],
+        [
+            InlineKeyboardButton(text="⚡Buy power", callback_data="buy_power"),
+        ]
     ]
     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
     refs = await UserDb.get_refs(message.chat.id)
+    balance = await UserDb.get_balance(message.chat.id)
     addr = ""
     if public_key == None:
         addr = "You have not connected your BTC address yet"
     else:
         addr = f"Connected Address:\n<code>{public_key}</code>"
-    await message.answer(f"🆔 <code>{message.chat.id}</code>\nTeraHash limit: {50+(refs*5)}\n{addr}", parse_mode="HTML", reply_markup=keyboard)
-
-@router.message(Text(text="Withdraw"))
-async def refs(message: Message):
-    kurs = []
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json') as response:
-            kurs = await response.json()
-    th = 0.00000006
-    refs = await UserDb.get_refs(message.chat.id)
-    hashrate = 50+(refs*5)
-    date_string = await UserDb.get_creation_time(message.chat.id)
-    datetime_object = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
-    time_passed = datetime.datetime.utcnow() - datetime_object
-    balance = format((th*hashrate)*(time_passed.total_seconds()/3600), ".8f")
-    public_key = await UserDb.get_public_key(message.chat.id)
-    buttons = [
-        [
-            InlineKeyboardButton(text="Withdraw", callback_data="set_wallet"),
-        ],
-    ]
-    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-    refs = await UserDb.get_refs(message.chat.id)
-    addr = ""
-    if public_key == None:
-        addr = "You have not connected your BTC address yet"
-    else:
-        addr = f"<code>{public_key}</code>"
-    await message.answer(f"You are going to withdraw your money to your connected address:\n{addr}\nYour balance:\n{balance} ({round(float(balance)*float(kurs['bpi']['USD']['rate_float']), 2)}$)", parse_mode="HTML", reply_markup=keyboard)
-
-@router.callback_query(Text(text='cancel'))
-async def refs(call: CallbackQuery, state: FSMContext):
-    await bot.send_message(call.message.chat.id, "Canceled")
-    await state.clear()
-
-@router.callback_query(Text(text='set_wallet'))
-async def refs(call: CallbackQuery, state: FSMContext):
-    buttons = [
-        [
-            InlineKeyboardButton(text="❌Cancel", callback_data="cancel"),
-        ],
-    ]
-    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-    await bot.send_message(call.message.chat.id, "Enter your Bitcoin address", reply_markup=keyboard)
-    await state.set_state(Addr_set.addr)
+    await message.answer(f"🆔 <code>{message.chat.id}</code>\n\nTeraHash limit: {50+(refs*5)}\n\nDeposit: {balance}$\n\n{addr}", parse_mode="HTML", reply_markup=keyboard)
 
 @router.message(Addr_set.addr)
 async def getid(message: Message, state: FSMContext):
@@ -155,12 +99,4 @@ async def getid(message: Message, state: FSMContext):
 
 @router.message(Text(text="Referals"))
 async def refs(message: Message):
-    refs = await UserDb.get_refs(message.chat.id)
     await message.answer(f"You can get 5 TeraHash for every referee you invite\nYour referal link: {await create_start_link(bot, str(message.chat.id))}")
-
-
-
-
-
-
-
