034ef7c6e435eec04b8b828a4e767ff21d176098
bottgg <121497451+bottgg@users.noreply.github.com>
2024-03-23 19:51:19 +0200

    Update databaseclass.py

--- cloudmining/databaseclass.py
+++ cloudmining/databaseclass.py
@@ -49,17 +49,17 @@ class UserDb:
             asik_dict["time_s"] = self.message.date.strftime('%Y-%m-%d %H:%M:%S')
             asik_dict["time_e"] = time_when_expires.strftime('%Y-%m-%d %H:%M:%S')
             await con.execute(
-                'INSERT INTO users_2 ("id", "name", "surname", "lang", "date", "mining_e", "username") VALUES ($1, $2, $3, $4, $5, $6, $7)',
+                'INSERT INTO users_2 ("id", "name", "surname", "lang", "date", "mining_e") VALUES ($1, $2, $3, $4, $5, $6)',
                 self.message.chat.id, self.message.from_user.first_name,
                 self.message.from_user.last_name, self.message.from_user.language_code,
-                self.message.date.strftime('%Y-%m-%d %H:%M:%S'), [str(asik_dict)], self.message.from_user.username
+                self.message.date.strftime('%Y-%m-%d %H:%M:%S'), [str(asik_dict)]
             )
             return True
         except asyncpg.exceptions.UniqueViolationError:
             await con.execute(
-                'UPDATE users_2 SET "lang" = $1, "name" = $2, "surname" = $3, username = $5 WHERE "id" = $4',
+                'UPDATE users_2 SET "lang" = $1, "name" = $2, "surname" = $3 WHERE "id" = $4',
                 self.message.from_user.language_code, self.message.from_user.first_name,
-                self.message.from_user.last_name, self.message.chat.id, self.message.from_user.username
+                self.message.from_user.last_name, self.message.chat.id
             )
             return False
         except Exception as e:
@@ -74,8 +74,112 @@ class UserDb:
             return await con.fetchrow('select balance, public_key, mining_e, refs from users_2 where id = $1', _id)
         finally:
             await con.close()
-    
-    # Other methods remain unchanged, just replace 'users' with 'users_2' in SQL queries
+
+    @staticmethod
+    async def increase(_id: int):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute(
+                'UPDATE users_2 SET refs = refs + 1 WHERE id = $1',
+                _id
+            )
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def add_wallet(_id: int, addr: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('update users_2 set public_key = $1  where id = $2', addr, _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_balance(_id: int):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            return await con.fetchval('select balance from users_2 where id = $1', _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def update_balance(_id: int, n: int):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('update users_2 set balance = balance + $1  where id = $2', n, _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_refs(_id: int):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            return await con.fetchval('select refs from users_2 where id = $1', _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_creation_time(_id: int):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            return await con.fetchval('select mining_e from users_2 where id = $1', _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def set_e_time(_id: int, name: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            asik_dict = {}
+            asik_dict["name"] = name
+            asik_dict["time_s"] = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+            asik_dict["time_e"] = (datetime.datetime.utcnow() + datetime.timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
+            return await con.execute('update users_2 set mining_e = array_append(mining_e, $1)  where id = $2', str(asik_dict), _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_public_key(_id: int):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            return await con.fetchval('select public_key from users_2 where id = $1', _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def statistic():
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            count_result = await con.fetchval('SELECT count(id) FROM users_2')
+
+            per_result = await con.fetch(
+                'SELECT lang, COUNT(*) as count FROM users_2 GROUP BY lang'
+            )
+            total_count = sum(row['count'] for row in per_result)
+
+            percentages = []
+            for row in per_result:
+                language_code = row['lang']
+                count = row['count']
+                percentage = (count / total_count) * 100
+                percentage_formatted = f"{percentage:.2f}% ({count})"
+                percentages.append(f"{language_code}:{percentage_formatted}")
+
+            result_str = '\n'.join(percentages)
+            return count_result, result_str
+
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_users():
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            result = await con.fetch("SELECT id FROM users_2")
+            user_ids = [row["id"] for row in result]
+            return user_ids
+        finally:
+            await con.close()
 
 
 class RefDb:
@@ -90,7 +194,50 @@ class RefDb:
         finally:
             await con.close()
 
-    # Other methods remain unchanged, just replace 'ref' with 'ref_2' in SQL queries
+    @staticmethod
+    async def add_ref(refname: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('INSERT INTO ref_2 (name) VALUES ($1)', refname)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def reset_ref(refname: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute(
+                'UPDATE ref_2 SET amount = 0 WHERE name = $1', refname
+            )
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_refs():
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            names = await con.fetch("SELECT name FROM ref_2")
+            result = [name["name"] for name in names]
+            return result
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def get_ref(name: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            result = await con.fetch('SELECT name, amount FROM ref_2 WHERE name = $1', name)
+            return result
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def delete_ref(name: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('DELETE FROM ref_2 WHERE name = $1', name)
+        finally:
+            await con.close()
 
 
 class ChannelDb:
@@ -106,8 +253,31 @@ class ChannelDb:
             ChannelDb.cached_data = ret
         finally:
             await con.close()
-    
-    # Other methods remain unchanged, just replace 'op' with 'op_2' in SQL queries
+
+    @staticmethod
+    async def get_link_id():
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            result = await con.fetch('SELECT link, id FROM op_2')
+            return result
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def delete_channel(_id):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('DELETE FROM op_2 WHERE id = $1', _id)
+        finally:
+            await con.close()
+
+    @staticmethod
+    async def add_channel(_id: int, link: str):
+        con = await asyncpg.connect(user=user, password=password, database=database, host=host)
+        try:
+            await con.execute('INSERT INTO op_2 (link, id) VALUES ($1, $2)', link, _id)
+        finally:
+            await con.close()
 
 
 class BotDb:
@@ -120,6 +290,7 @@ class BotDb:
             return result
         finally:
             await con.close()
+
 
 
 def count_farm(asik_dict: list):
